34f6a1f9133efa650f81fe277ecf9401f5d48394
vramirez122000 <bitingo80>
2015-12-06 09:33:26 -0400

    Progreso digitalización de rutas de Caguas

--- db/tuapp_caguas/routes.sql
+++ db/tuapp_caguas/routes.sql
@@ -1,4 +1,6 @@
-INSERT INTO ref.route_new (id,"desc",color,gpsenabled, sort_order, priority) VALUES
-  ('SL','San Lorenzo','#4d8327',true,1,1),
-  ('VALLE_TOLIMA','Urb. Valle Tolima','#bf6a4b',true,2,2)
+INSERT INTO ref.route_new (id, "desc", color, gpsenabled, sort_order, priority) VALUES
+  ('BC', 'Bo. Cañaboncito', '#e561de', TRUE, 1, 1),
+  ('BM', 'Bo. La Mesa / La Barra', '#5a82e8', TRUE, 2, 1),
+  ('VT', 'Urb. Valle Tolima', '#bf6a4b', TRUE, 3, 1),
+  ('SL', 'San Lorenzo', '#4d8327', TRUE, 4, 1)
 ;
