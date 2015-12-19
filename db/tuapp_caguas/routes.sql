396808eba2e58734f4cbce86eb731c1a3e019c50
vramirez122000 <bitingo80>
2015-12-19 13:57:12 -0400

    Added More Caguas routes

--- db/tuapp_caguas/routes.sql
+++ db/tuapp_caguas/routes.sql
@@ -1,7 +1,13 @@
 INSERT INTO ref.route_new (id, "desc", color, gpsenabled, sort_order, priority) VALUES
-  ('BC', 'Bo. Cañaboncito', '#9AC150', TRUE, 1, 1),
-  ('BM', 'Bo. La Mesa / La Barra', '#BC53B3', TRUE, 2, 1),
-  ('BO', 'Urb. Bonneville', '#C5604A', TRUE, 3, 1),
-  ('VT', 'Urb. Valle Tolima', '#94C2AE', TRUE, 4, 1),
-  ('AB', 'Aguas Buenas', '#544838', TRUE, 5, 1),
-  ('SL', 'San Lorenzo', '#7E75AA', TRUE, 6, 1);
+  ('BQ', 'Bo. Borinquen', '#CC4FC5', TRUE, 1, 1),
+  ('BC', 'Bo. Cañaboncito', '#6EB245', TRUE, 2, 1),
+  ('BM', 'Bo. La Mesa / La Barra', '#D45035', TRUE, 3, 1),
+  ('T1', 'Bo. Tomás de Castro 1', '#7797C7', TRUE, 4, 1),
+  ('T2', 'Bo. Tomás de Castro 2', '#C4943A', TRUE, 5, 1),
+  ('PC', 'Plaza Centro', '#C13E6C', TRUE, 6, 1),
+  ('BO', 'Urb. Bonneville', '#50A091', TRUE, 7, 1),
+  ('VT', 'Urb. Valle Tolima', '#4D6C34', TRUE, 8, 1),
+  ('AB', 'Aguas Buenas', '#7F6DCE', TRUE, 9, 1),
+  ('SL', 'San Lorenzo', '#5C4970', TRUE, 10, 1),
+  ('CI', 'Cidra', '#874B32', TRUE, 11, 1),
+  ('GU', 'Gurabo', '#C97CA3', TRUE, 12, 1);
