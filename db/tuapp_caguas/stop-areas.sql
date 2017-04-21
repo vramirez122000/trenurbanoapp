6688f9af197be0138c9d114425672627ce5898c0
vramirez122000 <bitingo80>
2017-04-20 23:33:28 -0400

    Rutas: Mariolga HIMA, Turabo Gardens, Villa Esperanza, Villas de Castro, Tomas de Castro 1 y 2, Barrio Cañaboncito, San Lorenzo

--- db/tuapp_caguas/stop-areas.sql
+++ db/tuapp_caguas/stop-areas.sql
@@ -1,9 +1,7 @@
 --No stop areas. Left blank
-/*with stop_area_tmp(id, "desc", sort_order, lng, lat) as (
-  select 'TERM_CAGUAS',     'Terminal Caguas',         1,  -66.032452, 18.235060 union
-  select 'TERM_SAN_LORENZO','Terminal San Lorenzo',    1,  -65.961513, 18.187426 union
-  select 'VALLE_TOLIMA',    'Valle Tolima',            1,  -66.046304, 18.242135
+with stop_area_tmp(id, "desc", sort_order, lng, lat) as (
+  select 'TERM_CAGUAS',   'Terminal Caguas',         1,  -66.032452, 18.235060 union
+  select 'GEORGETTI',     'Calle Georgetti',         1,  -66.035114, 18.231229
 )
 insert into ref.stop_area (id, "desc", lng, lat, geom, sort_order)
-  select id, "desc", lng, lat, ST_SetSRID(ST_Point(lng, lat), 4326), sort_order from stop_area_tmp;*/
-select 1;
+  select id, "desc", lng, lat, ST_SetSRID(ST_Point(lng, lat), 4326), sort_order from stop_area_tmp;
