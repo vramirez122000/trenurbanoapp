e26a40dae2628b6fc1398abbd453374f244166c9
vramirez122000 <bitingo80>
2017-04-23 14:19:07 -0400

    Itinerarios: Hormigas, La Changa, San Antonio, Beatriz

--- db/tuapp_caguas/sched/lachanga.sql
+++ db/tuapp_caguas/sched/lachanga.sql
@@ -1,35 +1,38 @@
--- Terminal de Caguas a Barrio Beatriz Lunes a Viernes
+
+-- Terminal de Caguas a Río Cañas, Sector La Changa, Lunes a Viernes
 with trip as (
     SELECT
-      'BE' route,
-      'GEORGETTI' stop_area,
-      'BEATRIZ' direction,
+      'CH' route,
+      'TERM_CAGUAS' stop_area,
+      'CHANGA' direction,
       'WORKDAY' schedule_type
 ), stop_times as (
-        SELECT TIME '05:00:00' stop_time
+  SELECT TIME '05:00:00' stop_time
+  UNION SELECT TIME '05:00:00'
   UNION SELECT TIME '06:30:00'
   UNION SELECT TIME '08:00:00'
   UNION SELECT TIME '09:30:00'
   UNION SELECT TIME '11:00:00'
   UNION SELECT TIME '12:30:00'
-  UNION SELECT TIME '13:30:00'
-  UNION SELECT TIME '15:00:00'
+  UNION SELECT TIME '14:00:00'
+  UNION SELECT TIME '15:30:00'
   UNION SELECT TIME '17:00:00'
   UNION SELECT TIME '17:10:00'
 ) INSERT INTO schedule (route, stop_area, direction, schedule_type, stop_time)
   select trip.route, trip.stop_area, trip.direction, trip.schedule_type, stop_times.stop_time
   from trip, stop_times order by stop_times.stop_time;
 
--- Terminal de Caguas a Barrio Beatriz Sabado y domingo
+-- Terminal de Caguas a Río Cañas, Sector La Changa, Sabado y domingo
 
 with trip as (
     SELECT
-      'BE' route,
-      'GEORGETTI' stop_area,
-      'BEATRIZ' direction,
+      'CH' route,
+      'TERM_CAGUAS' stop_area,
+      'CHANGA' direction,
       'RESTDAY' schedule_type
 ), stop_times as (
-        SELECT TIME '07:00:00' stop_time
+  SELECT TIME '07:00:00' stop_time
+  UNION SELECT TIME '07:00:00'
   UNION SELECT TIME '08:30:00'
   UNION SELECT TIME '10:00:00'
   UNION SELECT TIME '11:30:00'
--- db/tuapp_caguas/stop-areas.sql
+++ db/tuapp_caguas/stop-areas.sql
@@ -1,7 +1,13 @@
 --No stop areas. Left blank
 with stop_area_tmp(id, "desc", sort_order, lng, lat) as (
-  select 'TERM_CAGUAS',   'Terminal Caguas',         1,  -66.032452, 18.235060 union
-  select 'GEORGETTI',     'Calle Georgetti',         1,  -66.035114, 18.231229
+        select 'TERM_CAGUAS',   'Terminal Caguas',                1,  -66.032452, 18.235060
+  union select 'GEORGETTI',     'Calle Georgetti',                2,  -66.035114, 18.231229
+  union select 'AGUAS_BUENAS',  'Aguas Buenas',                   3,  -66.035114, 18.231229
+  union select 'BAIROA',        'Bo. Bairoa, Antigua Vía 25',     3,  -66.035114, 18.231229
+  union select 'BEATRIZ',       'Bo. Beatriz',                    3,  -66.035114, 18.231229
+  union select 'BONNEVILLE',    'Urb. Bonneville',                3,  -66.035114, 18.231229
+  union select 'CHANGA',        'Bo. Río Cañas, Sect. La Changa', 3,  -66.035114, 18.231229
+  union select 'HORMIGAS',      'Hormigas',                       3,  -66.035114, 18.231229
 )
 insert into ref.stop_area (id, "desc", lng, lat, geom, sort_order)
   select id, "desc", lng, lat, ST_SetSRID(ST_Point(lng, lat), 4326), sort_order from stop_area_tmp;
