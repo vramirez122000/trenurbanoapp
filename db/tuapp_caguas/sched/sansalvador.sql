974fcf92e99dd0298faee5d699630b01659acabe
vramirez122000 <bitingo80>
2017-04-28 19:50:04 -0400

    itinerario de viajes, ruta San Salvador

---
+++ db/tuapp_caguas/sched/sansalvador.sql
@@ -0,0 +1,42 @@
+
+-- Terminal de Caguas a Bo. San Antonio, Sect. Salchichón, Lunes a Viernes
+with trip as (
+    SELECT
+      'SS' route,
+      'TERM_CAGUAS' stop_area,
+      'SAN_SALVADOR' direction,
+      'WORKDAY' schedule_type
+), stop_times as (
+  SELECT TIME '05:00:00' stop_time
+  UNION SELECT TIME '06:30:00'
+  UNION SELECT TIME '08:00:00'
+  UNION SELECT TIME '09:30:00'
+  UNION SELECT TIME '11:00:00'
+  UNION SELECT TIME '12:30:00'
+  UNION SELECT TIME '14:00:00'
+  UNION SELECT TIME '15:30:00'
+  UNION SELECT TIME '17:00:00'
+  UNION SELECT TIME '17:10:00'
+) INSERT INTO schedule (route, stop_area, direction, schedule_type, stop_time)
+  select trip.route, trip.stop_area, trip.direction, trip.schedule_type, stop_times.stop_time
+  from trip, stop_times order by stop_times.stop_time;
+
+-- Terminal de Caguas a Bo. San Antonio, Sect. Salchichón, Sabado y domingo
+
+with trip as (
+    SELECT
+      'SS' route,
+      'TERM_CAGUAS' stop_area,
+      'SAN_SALVADOR' direction,
+      'RESTDAY' schedule_type
+), stop_times as (
+  SELECT TIME '07:00:00' stop_time
+  UNION SELECT TIME '08:30:00'
+  UNION SELECT TIME '10:00:00'
+  UNION SELECT TIME '11:30:00'
+  UNION SELECT TIME '13:00:00'
+  UNION SELECT TIME '14:30:00'
+  UNION SELECT TIME '16:00:00'
+) INSERT INTO schedule (route, stop_area, direction, schedule_type, stop_time)
+  select trip.route, trip.stop_area, trip.direction, trip.schedule_type, stop_times.stop_time
+  from trip, stop_times order by stop_times.stop_time;
