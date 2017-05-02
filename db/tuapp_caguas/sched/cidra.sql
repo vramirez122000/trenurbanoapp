69d34de768274d2ba51df57ea37749de45811c58
vramirez122000 <bitingo80>
2017-05-01 23:15:15 -0400

    Itinerario de Ruta: Cidra, Villa Esperanza

--- db/tuapp_caguas/sched/cidra.sql
+++ db/tuapp_caguas/sched/cidra.sql
@@ -1,4 +1,5 @@
--- Terminal de Caguas a Urb. Bonneville Lunes a Viernes
+
+-- Terminal de Caguas a Cidra Lunes a Viernes
 with trip as (
     SELECT
       'CI' route,
@@ -6,43 +7,44 @@ with trip as (
       'CIDRA' direction,
       'WORKDAY' schedule_type
 ), stop_times as (
-        SELECT TIME '05:00:00' stop_time
-  UNION SELECT TIME '05:20:00'
-  UNION SELECT TIME '05:40:00'
-  UNION SELECT TIME '06:00:00'
-  UNION SELECT TIME '06:20:00'
-  UNION SELECT TIME '06:40:00'
-  UNION SELECT TIME '07:00:00'
-  UNION SELECT TIME '07:20:00'
-  UNION SELECT TIME '07:40:00'
-  UNION SELECT TIME '08:00:00'
-  UNION SELECT TIME '08:30:00'
-  UNION SELECT TIME '09:00:00'
-  UNION SELECT TIME '09:30:00'
-  UNION SELECT TIME '10:00:00'
-  UNION SELECT TIME '10:30:00'
-  UNION SELECT TIME '11:00:00'
-  UNION SELECT TIME '11:30:00'
-  UNION SELECT TIME '12:00:00'
-  UNION SELECT TIME '12:30:00'
-  UNION SELECT TIME '13:00:00'
-  UNION SELECT TIME '13:30:00'
-  UNION SELECT TIME '14:00:00'
-  UNION SELECT TIME '14:30:00'
-  UNION SELECT TIME '15:00:00'
-  UNION SELECT TIME '15:30:00'
-  UNION SELECT TIME '16:00:00'
-  UNION SELECT TIME '16:30:00'
-  UNION SELECT TIME '17:00:00'
-  UNION SELECT TIME '17:10:00'
-  UNION SELECT TIME '18:00:00'
-  UNION SELECT TIME '18:10:00'
+  select '05:00:00' stop_time
+  union select '05:20:00'
+  union select '05:40:00'
+  union select '06:00:00'
+  union select '06:20:00'
+  union select '06:40:00'
+  union select '07:00:00'
+  union select '07:20:00'
+  union select '07:40:00'
+  union select '08:00:00'
+  union select '08:30:00'
+  union select '09:00:00'
+  union select '09:30:00'
+  union select '10:00:00'
+  union select '10:30:00'
+  union select '11:00:00'
+  union select '11:30:00'
+  union select '12:00:00'
+  union select '12:30:00'
+  union select '13:00:00'
+  union select '13:30:00'
+  union select '14:00:00'
+  union select '14:30:00'
+  union select '15:00:00'
+  union select '15:30:00'
+  union select '16:00:00'
+  union select '16:30:00'
+  union select '17:00:00'
+  union select '17:10:00'
+  union select '18:00:00'
+  union select '18:10:00'
+
 ) INSERT INTO schedule (route, stop_area, direction, schedule_type, stop_time)
   select trip.route, trip.stop_area, trip.direction, trip.schedule_type, stop_times.stop_time
   from trip, stop_times order by stop_times.stop_time;
 
--- Terminal de Caguas a Barrio Beatriz Sabado y domingo
 
+-- Terminal de Caguas a Cidra Fin de Semana
 with trip as (
     SELECT
       'CI' route,
@@ -50,13 +52,14 @@ with trip as (
       'CIDRA' direction,
       'RESTDAY' schedule_type
 ), stop_times as (
-        SELECT TIME '07:30:00' stop_time
-  UNION SELECT TIME '09:00:00'
-  UNION SELECT TIME '10:30:00'
-  UNION SELECT TIME '12:00:00'
-  UNION SELECT TIME '13:45:00'
-  UNION SELECT TIME '15:00:00'
-  UNION SELECT TIME '16:00:00'
+  select '07:30:00' stop_time
+  union select '09:00:00'
+  union select '10:30:00'
+  union select '12:00:00'
+  union select '13:45:00'
+  union select '15:00:00'
+  union select '16:00:00'
+
 ) INSERT INTO schedule (route, stop_area, direction, schedule_type, stop_time)
   select trip.route, trip.stop_area, trip.direction, trip.schedule_type, stop_times.stop_time
   from trip, stop_times order by stop_times.stop_time;
